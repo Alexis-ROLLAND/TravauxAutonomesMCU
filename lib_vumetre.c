cd9f869ee88d8ae5d5933ec5279722bd03089315
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2025-01-21 11:44:34 +0100

    MCU 05 (unit test) OK

--- lib_vumetre.c
+++ lib_vumetre.c
@@ -1,8 +1,8 @@
 /**
- * @file Template_lib_Appli_C.c 
+ * @file    lib_vumetre.c
  * @author 	Alexis ROLLAND
- * @date	2021-12-30
- * @brief 	Template for PIC24 main app
+ * @date	2025/01
+ * @brief 	
  *  
  *
  */
--- lib_vumetre.h
+++ lib_vumetre.h
@@ -1,8 +1,8 @@
 /**
- * @file Template_lib_Appli_H.h 
+ * @file    lib_vumetre.h 
  * @author 	Alexis ROLLAND
- * @date	2021-12-30
- * @brief 	Template for PIC24 main app
+ * @date	2025/01
+ * @brief 	
  *  
  *
  */
@@ -16,16 +16,16 @@
 #include <libpic30.h>
 #include <xc.h>
 
-//#define TEST_PILOTER_LEDS   /**< Unit test for "PiloterLEDs" task   */
+#define TEST_PILOTER_LEDS   /**< Unit test for "PiloterLEDs" task   */
 //#define TEST_GENE_TIC       /**< Unit test for timer config, and periodic ISR (T = 50ms) */
 //#define TEST_ACQ_VPOT       /**< Unit test for ADC config and End Of Conversion ISR */
-#define TEST_TRANSCODER       /**< Unit test for Transcoder function  */
+//#define TEST_TRANSCODER       /**< Unit test for Transcoder function  */
 
 #define PORT_LEDS   LATA
 #define TRIS_LEDS   TRISA
 
 #define PiloterLEDs(EtatLeds)   {PORT_LEDS = EtatLeds;}
-#define StartConversion()       {AD1CON1bits.SAMP = 0;}          // Start Conversion (force end of sample)
+#define StartConversion()       {AD1CON1bits.SAMP = 0;}          /**< Start Conversion (force end of sample) */
 
 #define NB_TIC_50MS         25000
 #define COEF_MISE_ECHELLE   ((float)(0.0087977))
@@ -41,11 +41,11 @@
 void Initialiser(void);
 
 /**
- * @brief  
+ * @brief   Transcoding function from read Voltage to Led pattern  
  * 
- * @param	
+ * @param	uint16_t Tension : Raw voltage, directly taken from ADC output register 
  * 
- * @return   
+ * @return  8 bit led pattern 
  *
  */
 uint8_t Transcoder(uint16_t Tension);
